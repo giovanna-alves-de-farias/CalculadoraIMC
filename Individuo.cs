0322971a1a8655e6f9f1296615944405dab0583e
Giovanna Alves de Farias <giovannaalves02@hotmail.com>
2022-04-23 21:48:32 -0300

    Mudança de Indentação

--- Individuo.cs
+++ Individuo.cs
@@ -1,11 +1,38 @@
-public class Individuo { // Uso de propriedades
+public class Individuo // Uso de propriedades
+{ 
 
-    public string Nome { get; set; }
-    
-    public double Peso { get; set; }
-
+    public String Nome { get; set; }
     public double Altura { get; set; }
+    public double Peso { get; set; }
+    public double Imc { get => Peso / (Altura * Altura); }
 
-    public double IMC { get; set; }
+    public Individuo(String nome, double altura, double peso)
+    {
+        Nome = nome;
+        Altura = altura;
+        Peso = peso;
+    }
 
+    public String ClassificacaoImc()
+    {
+        if(Imc <= 18.5)
+            return "Abaixo do Peso";
+
+        if(Imc >= 18.5 && Imc <= 24.9)
+            return "Peso Normal";
+
+        if(Imc >= 25 && Imc <= 29.9)
+            return "Sobrepeso";
+
+        if(Imc >= 30 && Imc <= 34.9)
+            return "Obesidade Grau I";
+
+        if(Imc >= 35 && Imc <= 39.9)
+            return "Obesidade Grau II";
+
+        else if (Imc >= 40)
+            return "Obesidade Grau III ou Mórbida";
+
+        return "";
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,43 +1,14 @@
-﻿using System;
-
-namespace CalculadoraIMC
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("\n Insira o seu nome");
-            string nome = Console.ReadLine();
+﻿Console.WriteLine("\n Insira o seu nome");
+string nome = Console.ReadLine();
  
-            Console.WriteLine("\n Insira a sua altura");
-            float altura = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("\n Insira a sua altura");
+float altura = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("\n Insira o seu peso em Kg. Ex: 67,7");
-            float peso = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("\n Insira o seu peso em Kg. Ex: 67,7");
+float peso = Convert.ToSingle(Console.ReadLine());
 
-            double imc = (peso/(altura*altura));
+Individuo i = new Individuo(nome, altura, peso);
 
-            Console.WriteLine("\n {0} A sua altura é {1} e o seu peso é {2}", nome, altura, peso);
-            Console.WriteLine("\n O seu IMC é {0}", imc);
-
-            // Informações da Tabela de IMC
-            if(imc <= 0){
-                Console.WriteLine("Esses valores são inválidos");
-            }else if(imc < 17){
-                Console.WriteLine("Você está muito abaixo do peso");
-            }else if((imc > 17) && (imc < 18.5)){
-                Console.WriteLine("Você está abaixo do peso");
-            }else if((imc > 18.5) && (imc < 25)){
-                Console.WriteLine("Você está com o peso normal");
-            }else if((imc > 25) && (imc < 30)){
-                Console.WriteLine("Você está acima do peso");
-            }else if((imc > 30) && (imc < 35)){
-                Console.WriteLine("Você está no grau de obesidade I");
-            }else if((imc > 35) && (imc < 40)){
-                Console.WriteLine("Você está no grau de Obesidade II, ou seja, severa");
-            }else  if (imc > 40){
-                Console.WriteLine("Obesidade Grau III, ou seja, mórbida");
-            }
-        }
-    }
-}
+Console.WriteLine("\n {0} você tem a altura é {1} e o peso é {2}", nome, altura, peso);
+Console.WriteLine("\n O seu IMC é {0}", i.Imc);
+Console.WriteLine(i.ClassificacaoImc());
